d9af8b2d1fda7ad10f2cd131bd5f7850204c2789
tawfikm <tawfikmourchid@gmail.com>
2019-02-11 11:58:52 +0100

    change x to 11

---
+++ Swap.java
@@ -0,0 +1,24 @@
+package DemoGit2;
+
+/**
+ * Created by MOURCHID Tawfik  on 2/11/2019,  at 11h42
+ */
+public class Swap {
+    public static  void  main(String args[]){
+
+        int x = 11;
+        int y = 20;
+
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+
+
+        System.out.println("swapping...");
+
+        // swapping
+
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+
+    }
+}
